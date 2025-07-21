9f18f59c021e1b729108ee348cb8a3e927ed8ab5
KL2400030537 <2400030537@kluniversity.in>
2025-07-21 15:27:42 +0530

    34

--- src/welcome.jsx
+++ src/welcome.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
+import Profile from './Profile';
+import About from './cre';
 class Welcome extends Component {
   render() {
     return (
       <div>
         <h1>Welcome Page</h1>
-        <a class="links"href="/Profile">Profile</a><br/>
-        <a class="links"href="/hidden/cre">Manhwa</a>
+        <Link className="links" to="/Profile">Profile</Link><br/>
+        <Link className="links" to="/hidden/cre">Manhwa</Link>
       </div>
     );
   }
